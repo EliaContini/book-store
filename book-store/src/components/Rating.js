c50dc045a403638f61a111cfaa65d5213a37b652
Elia Contini <elia.contini@gmail.com>
2021-03-19 14:39:24 +0100

    Rating component

--- book-store/src/components/Rating.js
+++ book-store/src/components/Rating.js
@@ -6,8 +6,37 @@
  * Renders the rating of a book store
  */
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import "./Rating.css";
+
 const Rating = (props) => {
-    return <div>{props.rating}</div>;
+    const maxRating = 5;
+    const rating = props.rating;
+
+    let content = [];
+    for (var i = 1; i <= maxRating; i++) {
+        if (i <= rating) {
+            content.push(
+                <FontAwesomeIcon
+                    icon={faStar}
+                    className="Rating-start-isOn"
+                    key={i}
+                />
+            );
+        } else {
+            content.push(
+                <FontAwesomeIcon
+                    icon={faStar}
+                    className="Rating-start"
+                    key={i}
+                />
+            );
+        }
+    }
+
+    return <div className="Rating">{content}</div>;
 };
 
 export default Rating;
